a88570fed254141dd9d61d91e151e11224c75d1b
魏培贤 <1076715530@qq.com>
2018-05-18 14:32:47 +0800

    delete

--- pages/add/add.js
+++ pages/add/add.js
@@ -106,33 +106,33 @@ Page({
             that.setData({
               token: res.data
             })
-            wx.request({
-              url: 'http://180.76.249.233:8080/newhelp/api/archiveStudent',
-              header: {
-                "Authorization": that.data.token,
-              },
-              data: {
-                studentId: that.data.sID,
-                teacherId: that.data.teacherid,
-                studyCondition: that.data.study,
-                studyCondition: that.data.study,
-                lifeCondition: that.data.life,
-                helpType: that.data.helpType,
-                bulidingBasis: that.data.base,
-                otherCondition: that.data.other,
-                healthCondition: that.data.healthy,
-                              familyCondition: that.data.family,
-                              name: that.data.name,
-                              bulidingPerson: that.data.teachername,
-                              bulidingPersonDuty: that.data.duty,
-                              attentionType: that.data.attention
-                            },
-                            method: "post",
-                            success: function (res) {
-                            if (res.data.success == true) {
-                              var that2 = res.data
-                              console.log(that2.message)
-                              wx.showToast({
+                          wx.request({
+                            url: 'http://180.76.249.233:8080/newhelp/api/archiveStudent',
+                           header: {
+                            "Authorization": that.data.token,
+                          },
+                          data: {
+                            studentId: that.data.sID,
+                            teacherId: that.data.teacherid,
+                            studyCondition: that.data.study,
+                            studyCondition: that.data.study,
+                            lifeCondition: that.data.life,
+                            helpType: that.data.helpType,
+                            bulidingBasis: that.data.base,
+                            otherCondition: that.data.other,
+                            healthCondition: that.data.healthy,
+                            familyCondition: that.data.family,
+                            name: that.data.name,
+                            bulidingPerson: that.data.teachername,
+                            bulidingPersonDuty: that.data.duty,
+                            attentionType: that.data.attention
+                          },
+                          method: "post",
+                          success: function (res) {
+                          if (res.data.success == true) {
+                            var that2 = res.data
+                            console.log(that2.message)
+                            wx.showToast({
                               title: res.data.message,
                               icon: "success"
                             })
--- pages/now/now.js
+++ pages/now/now.js
@@ -7,79 +7,14 @@ Page({
     teacherid: '',
     url: '',
     array1: ['年级', '2013级', '2014级', '2015级', '2016级', '2017级'],
-    objectArray: [
-      {
-        id: 0,
-        name: '年级'
-      },
-      {
-        id: 1,
-        name: '2013级'
-      },
-      {
-        id: 2,
-        name: '2014级'
-      },
-      {
-        id: 3,
-        name: '2015级'
-      },
-      {
-        id: 4,
-        name: '2016级'
-      },
-      {
-        id: 5,
-        name: '2017级'
-      }
-    ],
+    value1: ['', '2013级', '2014级', '2015级', '2016级', '2017级'],
     array2: ['帮扶类型', '家庭困难', '学业困难', '心理困难'],
-    objectArray: [
-      {
-        id: 0,
-        name: '帮扶类型'
-      },
-      {
-        id: 1,
-        name: '家庭困难'
-      },
-      {
-        id: 2,
-        name: '学业困难'
-      },
-      {
-        id: 3,
-        name: '心理困难'
-      }
-    ],
-    array4:['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17'],
+    value2: ['', '家庭困难', '学业困难', '心理困难'],
     array3: ['关注状态', '一般关注', '重点关注', '非常关注', '特殊关注'],
-    objectArray: [
-      {
-        id: 0,
-        name: '关注状态'
-      },
-      {
-        id: 1,
-        name: '一般关注'
-      },
-      {
-        id: 2,
-        name: '重点关注'
-      },
-      {
-        id: 3,
-        name: '非常关注'
-      },
-      {
-        id: 4,
-        name: '特殊关注'
-      }
-    ],
+    value3: ['', '一般关注', '重点关注', '非常关注', '特殊关注'],
     index1:0,
     index2:0,
     index3:0,
- token:'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0IiwiaWF0IjoxNTI1MDkwMzYyLCJpc3MiOiJTaGVuIn0dhMN_vBBmk16Sa3EOwIHnWNET-smmvXeAooRlxxvEtLzjpnrX-Fo-e01yKIhmZoj0XCNLw9nwD1CXPPlBrqmUw'
   },
 
   /**
@@ -87,61 +22,8 @@ Page({
    */
   onLoad: function (options) {
     var that=this;
-    var that = this;
-    wx.getStorage({
-      key: 'teacherId',
-      success: function (res) {
-        console.log(res.data)
-        that.setData({
-          teacherid: res.data
-        })
-        var arr = [
-          'http://180.76.249.233:8080/newhelp/api/archiveStudents/',
-          that.data.teacherid
-        ]
-        var str = arr.join('')
-        that.setData({
-          url: str
-        })
-        wx.getStorage({
-          key: 'token',
-          success: function (res) {
-            that.setData({
-              token: res.data
-            })
-            wx.request({
-      url:that.data.url,
-      header: {
-        "Authorization": that.data.token
-      },
-      method: "GET",
-      success: function (res) {
-        if (res.data.success == true) {
-          var that2 = res.data
-          console.log(that2.data)
-          that.setData({
-            studentlist:that2.data
-          })
-          
-        }
-        else {
-          console.log(res.data)
-          wx.showToast({
-            title: "连接失败",
-            icon: "fail"
-          })
-        }
-      },
-      fail: function (res) {
-        console.log(res);
-        console.log(1)
-      }
-    })
-          },
-
-        })
-      },
-    })
+    that.getlist()
+    
   },
 
   /**
@@ -197,6 +79,64 @@ Page({
       url: '../add/add'
     })
   },
+
+  getlist:function(){
+    var that = this;
+    wx.getStorage({
+      key: 'teacherId',
+      success: function (res) {
+        console.log(res.data)
+        that.setData({
+          teacherid: res.data
+        })
+        var arr = [
+          'http://180.76.249.233:8080/newhelp/api/archiveStudents/',
+          that.data.teacherid
+        ]
+        var str = arr.join('')
+        that.setData({
+          url: str
+        })
+        wx.getStorage({
+          key: 'token',
+          success: function (res) {
+            that.setData({
+              token: res.data
+            })
+            wx.request({
+              url: that.data.url,
+              header: {
+                "Authorization": that.data.token
+              },
+              method: "GET",
+              success: function (res) {
+                if (res.data.success == true) {
+                  var that2 = res.data
+                  console.log(that2.data)
+                  that.setData({
+                    studentlist: that2.data
+                  })
+
+                }
+                else {
+                  console.log(res.data)
+                  wx.showToast({
+                    title: "连接失败",
+                    icon: "fail"
+                  })
+                }
+              },
+              fail: function (res) {
+                console.log(res);
+                console.log(1)
+              }
+            })
+          },
+
+        })
+      },
+    })
+  },
   message: function (e) {
     wx.navigateTo({
       url: '../record/record'
@@ -207,24 +147,70 @@ Page({
       data: stuid,
     })
   },
-  
-
   bindPickerChange1: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
       index1: e.detail.value
     })
+    this.search() 
   },
   bindPickerChange2: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
       index2: e.detail.value
     })
+    this.search() 
   },
   bindPickerChange3:function(e){
     console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
       index3: e.detail.value
     })
+    this.search() 
+  },
+  search:function(){
+    var that =this
+    wx.getStorage({
+      key: 'teacherId',
+      success: function(res) {
+        that.setData({
+          teacherid: res.data
+        })
+        console.log(res.data)
+        if ((that.data.index1 != 0) || (that.data.index2 != 0) || (that.data.index3!=0)){
+        wx.request({
+          url: 'http://180.76.249.233:8080/newhelp/api/archiveStudents',
+          header: {
+            "Authorization": that.data.token,
+          },
+          data: {
+            teacherId: that.data.teacherid,
+            grade:that.data.value1[that.data.index1],
+            helpType: that.data.value2[that.data.index2],
+            attentionType: that.data.value3[that.data.index3],
+          },
+          method: "post",
+          success: function (res) {
+            
+            if (res.data.success == true) {
+              that.setData({
+                studentlist:res.data.data
+              })
+              console.log(res.data.data)
+            }
+            else {
+              console.log(res.data)
+            }
+          },
+          fail:function(res){
+            console.log(res.data)
+          }
+        })
+      }
+      else{
+        that.getlist()
+      }
+    }
+  })
   }
 })
